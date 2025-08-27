1251903f573cffe1c6fab9a55b17663647245b98
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-27 15:42:46 -0300

    add codigo teste

--- src/br/com/fiap/exemplo/view/Main.java
+++ src/br/com/fiap/exemplo/view/Main.java
@@ -1,7 +1,14 @@
 package br.com.fiap.exemplo.view;
 
+import br.com.fiap.exemplo.model.Bebida;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Implementing.");
+        Bebida bebida = new Bebida("Suco de Laranja", "Água, laranja, açúcar, gelo", 7.85, 500);
+        System.out.print("A bebida informada foi " + bebida.getNome());
+        System.out.print(", é composta por " + bebida.getIngredientes());
+        System.out.print(", custa R$" + bebida.getPreco());
+        System.out.print(" e seu tamanho é de " + bebida.getTamanho() + "ml");
     }
 }
+
