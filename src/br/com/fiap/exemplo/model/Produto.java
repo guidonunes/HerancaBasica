b81a4dbae192e040ea9fde1bdc3d4f5dd80b8934
Guilherme Nunes <guilherme.augd@gmail.com>
2025-08-27 14:53:57 -0300

    Exemplo de herança

---
+++ src/br/com/fiap/exemplo/model/Produto.java
@@ -0,0 +1,39 @@
+package br.com.fiap.exemplo.model;
+
+public class Produto {
+    private String nome;
+    private String ingredientes;
+    private Double preco;
+
+    public Produto(){}
+
+    public Produto(String nome, String ingredientes, Double preco) {
+        this.nome = nome;
+        this.ingredientes = ingredientes;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
